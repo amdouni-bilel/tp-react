1507e8860a6fd39571f1ccd9316095a53f6aafe8
amdouni-bilel <amdounibilel1@gmail.com>
2025-11-29 13:14:15 +0100

    UseEffect Example

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Counter from './components/Counter';
 import LifeCycleClass from './components/LifeCycleClass';
 import Welcome from './components/Welcome';
 import UseStateHooks from './components/UseStateHooks';
+import UseEffectHooks from './components/UseEffectHooks';
 
 function App() {
   const [showLifeCycle, setShowLifeCycle] = useState(true);
 
   return (
     <div className="App">
-        <UseStateHooks />
-      
+        <UseEffectHooks />
 
     </div>
   );
